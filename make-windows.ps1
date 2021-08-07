25302dc5b367ac09da89448836e9bb2825bf1ee3
Damien LeBerrigaud <git@itsdamo.com>
2021-08-07 13:24:59 -0600

    Upgrade to .NET 5

--- make-windows.ps1
+++ make-windows.ps1
@@ -3,5 +3,5 @@ rm -r -fo Cli/obj
 
 dotnet build
 dotnet publish Cli -c Release -r win-x64
-warp-packer --arch windows-x64 --input_dir Cli/bin/Release/netcoreapp2.2/win-x64/publish --exec Cli.exe --output Cli/bin/Release/photos-organizer.exe
+warp-packer --arch windows-x64 --input_dir Cli/bin/Release/net5/win-x64/publish --exec Cli.exe --output Cli/bin/Release/photos-organizer.exe
 dotnet build
